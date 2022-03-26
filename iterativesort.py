eec7788f3e811829bf2c84161ef9e4c9f4e6dbdc
CHRIS_LEE <chrismlee87@gmail.com>
2022-03-25 17:23:18 -0700

    [add] implement bubble sort

---
+++ iterativesort.py
@@ -0,0 +1,43 @@
+def is_sorted(items):
+    """Return a boolean indicating whether given items are in sorted order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Check that all adjacent items are in order, return early if so
+
+
+def bubble_sort(items):
+    """Sort given items by swapping adjacent items that are out of order, and
+    repeating until all items are in sorted order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Repeat until all items are in sorted order
+    # TODO: Swap adjacent items that are out of order
+    for i in range(len(items)):  # outer loop
+        for j in range(len(items) - 1):  # inner loop, compare integers
+            if items[j] > items[j+1]:
+                items[j], items[j+1] = items[j+1], items[j]
+
+
+items = [3, 1, 7, 0]
+bubble_sort(items)
+print(items)
+
+
+def selection_sort(items):
+    """Sort given items by finding minimum item, swapping it with first
+    unsorted item, and repeating until all items are in sorted order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Repeat until all items are in sorted order
+    # TODO: Find minimum item in unsorted items
+    # TODO: Swap it with first unsorted item
+
+
+def insertion_sort(items):
+    """Sort given items by taking first unsorted item, inserting it in sorted
+    order in front of items, and repeating until all items are in order.
+    TODO: Running time: ??? Why and under what conditions?
+    TODO: Memory usage: ??? Why and under what conditions?"""
+    # TODO: Repeat until all items are in sorted order
+    # TODO: Take first unsorted item
+    # TODO: Insert it in sorted order in front of items
